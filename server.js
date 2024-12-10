6b288d8b2ecfb5835edbece85682ea295bb5a059
mateus443 <mateusp2110@gmail.com>
2024-12-10 13:08:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors'); // Para habilitar CORS se precisar de APIs externas
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Habilitar CORS, caso necessário (para chamadas de APIs externas)
+app.use(cors());
 
 // Servir arquivos estáticos da pasta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
@@ -10,6 +14,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Rota principal para servir o index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Exemplo de uma rota adicional para a página 'sobre'
+app.get('/sobre', (req, res) => {
+    res.sendFile(path.join(__dirname, 'sobre.html')); // Supondo que você crie essa página
+});
+
+// Middleware para capturar erros 404 (página não encontrada)
+app.use((req, res, next) => {
+    res.status(404).send('Página não encontrada!');
 });
 
 // Iniciar o servidor
